96a5d4734f6430b243da3abd8515d4716bceb76c
Dierk Koenig <dierk.koenig@canoo.com>
2018-11-12 12:24:10 +0100

    add common security setup to bootstrap

--- grails-app/init/mvc/BootStrap.groovy
+++ grails-app/init/mvc/BootStrap.groovy
@@ -1,6 +1,9 @@
 package mvc
 
 import grails.util.Environment
+import mvs.SecRole
+import mvs.SecUser
+import mvs.SecUserSecRole
 
 import javax.security.auth.login.FailedLoginException
 
@@ -8,26 +11,36 @@ class BootStrap {
 
     def init = { servletContext ->
 
+        // in production or test, this might already be in the DB
+        SecRole adminRole = save(SecRole.findOrCreateWhere(authority: 'ROLE_ADMIN'))
+
         if(Environment.current != Environment.DEVELOPMENT) { // guard clause
             return;
         }
 
-        Person dierk = new Person(firstName: "Dierk", lastName: "König", email: "dierk.koenig@fhnw.ch")
-        dierk.save(flush: true, failOnError: true)
+        SecUser testUser  = save(new SecUser(username: 'me', password: 'toobad'))
+        SecUserSecRole.create(testUser, adminRole, true) //flush
 
-        for(int i = 0; i<1000; i++) {
-            Person p = new Person(firstName: "first"+i, lastName: "last"+i, email: "a@b.com")
-            p.save(failOnError: true)
+        // plausibility check
+        assert SecRole.count()          == 1
+        assert SecUser.count()          == 1
+        assert SecUserSecRole.count()   == 1
+
+        Person dierk = save(new Person(firstName: "Dierk", lastName: "König", email: "dierk.koenig@fhnw.ch"))
+
+        for(int i = 0; i<10; i++) {
+            save(new Person(firstName: "first"+i, lastName: "last"+i, email: "a@b.com"))
         }
 
-        Room r = new Room(description: "5.4D11", capacity: 40)
-        r.save(failOnError: true);
+        Room r = save(new Room(description: "5.4D11", capacity: 40))
 
         Date today = new Date().clearTime()
-        Booking b = new Booking(booker: dierk, room: r, date: today, slot: Booking.AM)
-        b.save(failOnError: true)
+        Booking b = save(new Booking(booker: dierk, room: r, date: today, slot: Booking.AM))
     }
 
+    static save(domainObject) {
+        domainObject.save(failOnError: true, flush:true)
+    }
 
     def destroy = {
     }
